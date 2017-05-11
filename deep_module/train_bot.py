fca6564b2f4c9ee8f9fa236c3f3a18bc3d8edd56
darial <darialdo92@gmail.com>
2017-05-11 17:16:44 -0500

    added code

--- deep_module/train_bot.py
+++ deep_module/train_bot.py
@@ -10,14 +10,15 @@ from keras.layers.convolutional import MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
-def pre_process_data(reviews, labels, polarity_cutoff = 0.1, min_count = 10):
-        
+
+# function to create word2index based on reviews in the dataset
+def pre_process_data(reviews, labels, polarity_cutoff=0.1, min_count=10):
         positive_counts = Counter()
         negative_counts = Counter()
         total_counts = Counter()
 
         for i in range(len(reviews)):
-            if(labels[i] == 'POSITIVE'):
+            if labels[i] == 'POSITIVE':
                 for word in reviews[i].split(" "):
                     positive_counts[word] += 1
                     total_counts[word] += 1
@@ -28,13 +29,13 @@ def pre_process_data(reviews, labels, polarity_cutoff = 0.1, min_count = 10):
 
         pos_neg_ratios = Counter()
 
-        for term,cnt in list(total_counts.most_common()):
-            if(cnt >= 50):
+        for term, cnt in list(total_counts.most_common()):
+            if cnt >= 50:
                 pos_neg_ratio = positive_counts[term] / float(negative_counts[term]+1)
                 pos_neg_ratios[term] = pos_neg_ratio
 
         for word,ratio in pos_neg_ratios.most_common():
-            if(ratio > 1):
+            if ratio > 1:
                 pos_neg_ratios[word] = np.log(ratio)
             else:
                 pos_neg_ratios[word] = -np.log((1 / (ratio + 0.01)))
@@ -42,9 +43,9 @@ def pre_process_data(reviews, labels, polarity_cutoff = 0.1, min_count = 10):
         review_vocab = set()
         for review in reviews:
             for word in review.split(" "):
-                if(total_counts[word] > min_count):
-                    if(word in pos_neg_ratios.keys()):
-                        if((pos_neg_ratios[word] >= polarity_cutoff) or (pos_neg_ratios[word] <= -polarity_cutoff)):
+                if total_counts[word] > min_count:
+                    if word in pos_neg_ratios.keys():
+                        if(pos_neg_ratios[word] >= polarity_cutoff) or (pos_neg_ratios[word] <= -polarity_cutoff):
                             review_vocab.add(word)
                     else:
                         review_vocab.add(word)
@@ -69,6 +70,14 @@ def pre_process_data(reviews, labels, polarity_cutoff = 0.1, min_count = 10):
         return review_vocab, label_vocab, word2index, label2index
 
 
+# convert string reviews to vectors
+def update_input_layer(review):
+    layer_0 = np.zeros((1, vocab_size))
+    for word in review.split(" "):
+        layer_0[0][word2index[word]] += 1
+    return layer_0
+
+
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
@@ -76,20 +85,18 @@ np.random.seed(seed)
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 
-# pad dataset to a maximum review length in words
+# pad data set to a maximum review length in words
 max_words = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_words)
 X_test = sequence.pad_sequences(X_test, maxlen=max_words)
 
-def pretty_print_review_and_label(i):
-    print(labels[i] + "\t:\t" + reviews[i][:80] + "...")
-
-g = open('reviews.txt','r') # What we know!
-reviews = list(map(lambda x:x[:-1],g.readlines()))
+# start code
+g = open('reviews.txt', 'r')  # What we know!
+reviews = list(map(lambda x: x[:-1],g.readlines()))
 g.close()
 
-g = open('labels.txt','r') # What we WANT to know!
-labels = list(map(lambda x:x[:-1].upper(),g.readlines()))
+g = open('labels.txt', 'r')  # What we WANT to know!
+labels = list(map(lambda x: x[:-1].upper(),g.readlines()))
 g.close()
 
 positive_counts = Counter()
@@ -97,7 +104,7 @@ negative_counts = Counter()
 total_counts = Counter()
 
 for i in range(len(reviews)):
-    if(labels[i] == 'POSITIVE'):
+    if labels[i] == 'POSITIVE':
         for word in reviews[i].split(" "):
             positive_counts[word] += 1
             total_counts[word] += 1
@@ -106,32 +113,23 @@ for i in range(len(reviews)):
             negative_counts[word] += 1
             total_counts[word] += 1
 
-print('1asd',reviews[0], labels[0])
+print('1asd', reviews[0], labels[0])
 
-print('1asd',reviews[10], labels[10])
+print('1asd', reviews[10], labels[10])
 
 vocab = set(total_counts.keys())
 vocab_size = len(vocab)
 print(vocab_size)
-
 word2index = {}
 
-for i,word in enumerate(vocab):
+for i, word in enumerate(vocab):
     word2index[word] = i
-word2index
-
-
-def update_input_layer(review):
-    layer_0 = np.zeros((1,vocab_size))
-    # clear out previous state, reset the layer to be all 0s
-    for word in review.split(" "):
-        layer_0[0][word2index[word]] += 1
-    return layer_0
 
 for k in range(4):
     l0 = update_input_layer(reviews[k])
-    print(l0[0],np.sum(l0[0]),len(l0[0]))
+    print(l0[0], np.sum(l0[0]), len(l0[0]))
 
+# pre-process text data
 '''
 # create the model
 model = Sequential()
